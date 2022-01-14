e564cddbf5e5dac847d65ff324608b1dff32eb20
Robson Klug <robsonklug@gmail.com>
2022-01-13 21:53:14 -0300

    Update

--- previsao.py
+++ previsao.py
@@ -1,13 +1,47 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
 
 tabela = pd.read_csv("advertising.csv")
 print(tabela)
 
 sns.heatmap(tabela.corr(), annot=True, cmap="Wistia")
-plt.show()
+#plt.show()
 
 # outra forma de ver a mesma análise
 # sns.pairplot(tabela)
 # plt.show()
+
+y = tabela["Vendas"]
+x = tabela.drop("Vendas", axis=1)
+
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3, random_state=1)
+
+
+# cria as inteligencias aritificiais
+modelo_regressaolinear = LinearRegression()
+modelo_arvoredecisao = RandomForestRegressor()
+# treina as inteligencias artificias
+modelo_regressaolinear.fit(x_treino, y_treino)
+modelo_arvoredecisao.fit(x_treino, y_treino)
+
+
+# criar as previsoes
+previsao_regressaolinear = modelo_regressaolinear.predict(x_teste)
+previsao_arvoredecisao = modelo_arvoredecisao.predict(x_teste)
+# comparar os modelos
+print(metrics.r2_score(y_teste, previsao_regressaolinear))
+print(metrics.r2_score(y_teste, previsao_arvoredecisao))
+
+
+tabela_auxiliar = pd.DataFrame()
+tabela_auxiliar["y_teste"] = y_teste
+tabela_auxiliar["Previsoes ArvoreDecisao"] = previsao_arvoredecisao
+tabela_auxiliar["Previsoes Regressao Linear"] = previsao_regressaolinear
+plt.figure(figsize=(15,6))
+sns.lineplot(data=tabela_auxiliar)
+plt.show()
